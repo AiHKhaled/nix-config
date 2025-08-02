54b9c59849d16bade4f7f43b810dc96abb5425fc
Correct author <aithammouk40@gmail.com>
2025-08-03 00:14:11 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "NixOS configuration";
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
